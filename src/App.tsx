87e74e7ef58b7530cd906791905de7f2c8174d23
Hanee (임하은) <hanee@aitrics.com>
2024-06-21 10:22:12 +0900

    refactor: Update Model component to use spring animations for position, scale, and rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import { Environment, useGLTF } from '@react-three/drei';
 import { Canvas, PrimitiveProps, useFrame } from '@react-three/fiber';
 import { useScroll, useSpring, useTransform } from 'framer-motion';
+import { useControls } from 'leva';
 import { useRef } from 'react';
 import './App.css';
 
 function Model({
-  progress,
-}: Omit<PrimitiveProps, 'object'> & {
-  progress: ReturnType<typeof useSpring>;
-}) {
+  x,
+  y,
+  z,
+  scale,
+  rotationX,
+  rotationY,
+  rotationZ,
+}: Omit<PrimitiveProps, 'object'>) {
   const ref = useRef<PrimitiveProps>();
   const { scene } = useGLTF('./model.gltf');
 
   useFrame(() => {
-    const [x, y, z, scale, rotationX, rotationY, rotationZ] = progress.get();
+    const xValue = x.get();
+    const yValue = y.get();
+    const zValue = z.get();
+
+    const scaleValue = scale.get();
+    const rotationXValue = rotationX.get();
+    const rotationYValue = rotationY.get();
+    const rotationZValue = rotationZ.get();
+
     if (ref.current) {
-      ref.current.position.set(x, y, z);
-      ref.current.scale.set(scale, scale, scale);
-      ref.current.rotation.set(rotationX, rotationY, rotationZ);
+      ref.current.position.set(xValue, yValue, zValue);
+      ref.current.scale.set(scaleValue, scaleValue, scaleValue);
+      ref.current.rotation.set(rotationXValue, rotationYValue, rotationZValue);
     }
   });
 
@@ -28,19 +41,47 @@ function Model({
 
 function App() {
   const container = useRef(null);
+  const { scene } = useGLTF('./model.gltf');
+
+  const {
+    scale: scaleControl,
+    rotationX: rotationXControl,
+    rotationY: rotationYControl,
+    rotationZ: rotationZControl,
+    ...position
+  } = useControls({
+    x: { value: -20, min: -100, max: 100 },
+    y: { value: 15, min: -100, max: 100 },
+    z: { value: 0, min: -100, max: 100 },
+    scale: { value: 1, min: 0, max: 2 },
+    rotationX: { value: 0, min: -Math.PI, max: Math.PI },
+    rotationY: { value: 0, min: -Math.PI, max: Math.PI },
+    rotationZ: { value: 0, min: -Math.PI, max: Math.PI },
+  });
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ['start start', 'end end'],
   });
-  const progress = useTransform(
+
+  const x = useTransform(scrollYProgress, [0, 0.5, 1], [-20, 20, -16]);
+  const y = useTransform(scrollYProgress, [0, 0.5, 1], [15, -15, -31]);
+  const z = useTransform(scrollYProgress, [0, 0.5, 1], [0, 0, 4]);
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1.5, 1.8]);
+  const rotationX = useTransform(scrollYProgress, [0, 1], [0, -0.5]);
+  const rotationY = useTransform(
     scrollYProgress,
-    [0, 1],
-    [
-      [-20, 15, 0, 0.5, 0, 0, 0],
-      [20, -15, 0, 1.5, 0, Math.PI / 2, 0], // 스크롤 중간 위치
-    ]
+    [0, 0.5, 1],
+    [0, Math.PI / 2, 2]
   );
-  const progressSpring = useSpring(progress, { damping: 40 });
+  const rotationZ = useTransform(scrollYProgress, [0, 1], [0, -0.5]);
+
+  const xSpring = useSpring(x, { damping: 40 });
+  const ySpring = useSpring(y, { damping: 40 });
+  const zSpring = useSpring(z, { damping: 30 });
+  const scaleSpring = useSpring(scale, { damping: 40 });
+  const rotationXSpring = useSpring(rotationX, { damping: 40 });
+  const rotationYSpring = useSpring(rotationY, { damping: 40 });
+  const rotationZSpring = useSpring(rotationZ, { damping: 40 });
 
   return (
     <main ref={container}>
@@ -52,7 +93,30 @@ function App() {
           <Environment preset="apartment" />
           <directionalLight intensity={2} position={[0, 2, 3]} />
           <ambientLight />
-          <Model progress={progressSpring} />
+          {/* <primitive
+            object={scene}
+            position={[position.x, position.y, position.z]}
+            scale={scaleControl}
+            rotation={[rotationXControl, rotationYControl, rotationZControl]}
+          /> */}
+          {/* <Model
+            x={x}
+            y={y}
+            z={z}
+            scale={scale}
+            rotationX={rotationX}
+            rotationY={rotationY}
+            rotationZ={rotationZ}
+          /> */}
+          <Model
+            x={xSpring}
+            y={ySpring}
+            z={zSpring}
+            scale={scaleSpring}
+            rotationX={rotationXSpring}
+            rotationY={rotationYSpring}
+            rotationZ={rotationZSpring}
+          />
           <axesHelper args={[8]} />
         </Canvas>
       </div>
